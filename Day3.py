7ff1428d9a314b9e6a87492e941a9ab83603287e
Bookisso <50346734+Bookisso@users.noreply.github.com>
2022-03-03 10:54:11 +0800

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -2,6 +2,7 @@
 身份验证
 '''
 
+'''
 username = input('请输入用户名：')
 password = input('请输入口令：')
 
@@ -9,3 +10,6 @@ if username == 'admin' and password == '123456' :
 	print('验证成功！')
 else :
 	print('验证失败！')
+'''
+
+
