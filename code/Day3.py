d97e00af8180f74248db15c489a1848527514eea
Bookisso <50346734+Bookisso@users.noreply.github.com>
2022-03-09 19:40:25 +0800

    1

--- code/Day3.py
+++ code/Day3.py
@@ -12,4 +12,21 @@ else :
 	print('验证失败！')
 '''
 
+'''
+练习1
+value = float(input("请输入长度："))
+unit = input("请输入单位：")
 
+if unit == "英寸" :
+	print(f'{value}英寸是{2.54 * value}厘米')
+elif unit == "厘米" :
+	print(f'{value}厘米是{0.39 * value}英寸')
+else :
+	print('输入错误')
+'''
+
+'''‘’‘
+练习2
+r = float(input("请输入半径："))
+print(f'圆的周长是{2 * 3.14 * r}，面积是{3.14 * r *r}')
+'''
---
+++ code/Day4.py
@@ -0,0 +1,11 @@
+'''
+for 循环
+'''
+
+sum = 0
+a = 'abc'
+a += '0'
+for i in range (3):
+	print(i)
+print(a,a[3])
+
